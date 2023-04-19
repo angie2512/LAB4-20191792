c2ed2e31a0b8f691eb20e70fd5868b3390a926e7
angie2512 <a20191792@pucp.edu.pe>
2023-04-18 20:27:47 -0500

    miprimercommit

---
+++ src/main/java/com/example/lab4_20191792/Lab420191792Application.java
@@ -0,0 +1,13 @@
+package com.example.lab4_20191792;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab420191792Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab420191792Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab4_20191792/Lab420191792ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab4_20191792;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab420191792ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
